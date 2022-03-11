3558211bd8e94c06c0902aabb425bc1233b937c9
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-11 14:24:36 +0100

    jsnack 3 end

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,3 +2,12 @@ console.log('js ok');
 //Crea due array che hanno un numero di elementi diversi.Aggiungi elementi casuali all’array che ha meno elementi,
 // fino a quando ne avrà tanti quanti l’altro.
 
+const pizzaArray = ['Margherita', 'Marinara', 'Diavola', 'Funghi', 'Salsiccia', 'Amatriciana'];
+const clientsArray = ['Antonio', 'Angelo', 'Alessio'];
+
+while (pizzaArray.length > clientsArray.length) {
+    clientsArray.push(prompt('Inserisci il nome del cliente'));
+}
+
+console.log(pizzaArray);
+console.log(clientsArray);
