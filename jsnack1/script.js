b9e2094f16e8b18cb451fb6ae2a48e87acc245c3
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-11 14:16:16 +0100

    js snack 2 end

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,15 +1,15 @@
-console.log('js ok');
+
 //Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
 
 const numIntArray = [2, 5, 8, 10, 25, 35];
 let sumOdds = 0;
 
-for (i = 0; i < numIntArray.length; i++){
+for (let i = 0; i < numIntArray.length; i++){
     if (i % 2 !== 0) {
         sumOdds += numIntArray[i];
     }
 }
 
-console.log(numIntArray);
+console.log(numIntArray); // log array to check easily
 
-console.log(sumOdds); //somma di 5 + 10 + 35 = 50 (posizione 1, 3, 5)
+console.log(sumOdds); //sum of 5 + 10 + 35 = 50 (index 1, 3, 5)
--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,3 +1,30 @@
 console.log('js ok');
 //Il software deve chiedere per 5 volte all’utente di inserire un numero.
 //Il programma stampa la somma di tutti i numeri inseriti.Esegui questo programma in due versioni, con il for e con il while.
+
+
+//for version start
+// let sum = 0;
+// for (let i = 0; i < 5; i++){
+//     let number = parseInt(prompt(`${i + 1}, inserisci un numero:`));
+//     sum += number;
+// }
+
+// console.log(`Sum is: ${sum}`);
+// for version end
+
+
+
+//while version start
+let i = 0;
+let sum = 0;
+while (i < 5) {
+    number = parseInt(prompt(`${i + 1}, inserisci un numero:`));
+    sum += number;
+    i++;
+}
+
+console.log(`Sum is: ${sum}`);
+//while version end
+
+
